35605ce33381012e561f01487bdb82a8ca05a5fc
Gabe Chavez <73260097+GabeCodes72@users.noreply.github.com>
2021-09-17 12:22:43 -0500

    Add main code to Rep
    Added "Code 9-17.py" to the github repository.

---
+++ Code 9-17.py
@@ -0,0 +1,35 @@
+import webbrowser
+import random
+import time
+rn = random.randrange(1,7) 
+
+if rn == 1:
+    webbrowser.open("https://google.com")
+    time.sleep(1)
+
+elif rn == 2:
+    webbrowser.open("https://stackoverflow.com/")
+    time.sleep(1)
+
+elif rn == 3:
+    webbrowser.open("https://microsoft.com")
+    time.sleep(1)
+
+elif rn == 4:
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    time.sleep(1)
+
+elif rn == 5:
+    webbrowser.open("https://bit.ly/SECRETFILES12")
+    time.sleep(1)
+
+elif rn == 6:
+    webbrowser.open("https://www.google.com/search?q=duck&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjT9rm_kYbzAhWSlmoFHTutCYIQ_AUoAXoECAEQAw&biw=1366&bih=625&dpr=1&safe=active&ssui=on")
+    time.sleep(1)
+
+elif rn == 7:
+    webbrowser.open("https://www.bing.com/images/search?view=detailV2&ccid=lVpPZmhQ&id=36A0043EA2CBDB01A3ED46D55FD9501A23F8AC47&thid=OIP.lVpPZmhQQ9bZE_9iaR6UUgHaHa&mediaurl=https%3a%2f%2fi.pinimg.com%2foriginals%2f50%2f3c%2ff6%2f503cf6422b7c7497c00895cd13424ec9.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.955a4f66685043d6d913ff62691e9452%3frik%3dR6z4IxpQ2V%252fVRg%26pid%3dImgRaw%26r%3d0&exph=800&expw=800&q=La+Croix+Flavors+Meme&simid=608045791618272906&FORM=IRPRST&ck=D066F3C41D3E9C21A3A950C6A3EA3A65&selectedIndex=2")
+    time.sleep(1)
+
+elif rn == 8:
+    webbrowser.open("")
