b0c3dca96d01cd649155ed8d067f1fe2761b1cb3
Gabe Chavez <73260097+GabeCodes72@users.noreply.github.com>
2021-09-17 13:54:00 -0500

    Updated Randoweb file

---
+++ Randoweb.py
@@ -0,0 +1,124 @@
+import webbrowser as web
+import random
+import time
+rn = random.randrange(1,15) 
+
+if rn == 1:
+    web.open("https://google.com")
+    time.sleep(1)
+
+elif rn == 2:
+    web.open("https://stackoverflow.com/")
+    time.sleep(1)
+
+elif rn == 3:
+    web.open("https://microsoft.com")
+    time.sleep(1)
+
+elif rn == 4:
+    web.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    time.sleep(1)
+
+elif rn == 5:
+    web.open("https://bit.ly/SECRETFILES12")
+    time.sleep(1)
+
+elif rn == 6:
+    web.open("https://www.google.com/search?q=duck&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjT9rm_kYbzAhWSlmoFHTutCYIQ_AUoAXoECAEQAw&biw=1366&bih=625&dpr=1&safe=active&ssui=on")
+    time.sleep(1)
+
+elif rn == 7:
+    web.open("La croix flavor meme")
+    time.sleep(1)
+
+elif rn == 8:
+    web.open("https://github.com/")
+    time.sleep(1)
+
+elif rn == 9:
+    web.open("https://facebook.com")
+    time.sleep(1)
+
+elif rn == 10:
+    web.open("https://instagram.com")
+    time.sleep(1)
+
+elif rn == 11:
+    web.open("https://Lego.com")
+    time.sleep(1)
+
+elif rn == 12:
+    web.open("https://walmart.com")
+    time.sleep(1)
+
+elif rn == 13:
+    web.open("https://gamestop.com")
+    time.sleep(1)
+
+elif rn == 14:
+    web.open("https://duckduckgo.com")
+    time.sleep(1)
+
+elif rn == 15:
+    web.open("https://inrupt.com")
+    time.sleep(1)
+
+elif rn == 16:
+    web.open("https://disney.com")
+    time.sleep(1)
+
+elif rn == 17:
+    web.open("https://www.facebook.com/This-is-the-page-that-never-ends-103455441214129/")
+    time.sleep(1)
+
+elif rn == 18:
+    web.open("https://minecraft.net")
+    time.sleep(1)
+
+elif rn == 19:
+    web.open("https://nasa.gov")
+    time.sleep(1)
+
+elif rn == 20:
+    web.open("https://wikipedia.org")
+    time.sleep(1)
+
+elif rn == 21:
+    web.open("https://en.wikipedia.org/wiki/House_(TV_series)")
+    time.sleep(1)
+
+elif rn == 22:
+    web.open("https://en.wikipedia.org/wiki/David_Sarnoff")
+    time.sleep(1)
+
+elif rn == 23:
+    web.open("https://en.wikipedia.org/wiki/Union_Stock_Yards")
+    time.sleep(1)
+
+elif rn == 24:
+    web.open("https://en.wikipedia.org/wiki/White-collar_crime")
+    time.sleep(1)
+
+elif rn == 25:
+    web.open("https://artsandculture.google.com/partner/hong-kong-heritage-museum")
+    time.sleep(1)
+
+elif rn == 26:
+    web.open("https://www.google.com/search?q=I%27m+Feeling+Curious&csf=b&ct=ifl&cad=2:curious&ei=NuJEYYLeGMOf-Ab365yABw&ved=0ahUKEwiC0eql1YbzAhXDD94KHfc1B3AQnRsIDw&rct=j&safe=active&ssui=on")
+    time.sleep(1)
+
+elif rn == 27:
+    web.open("http://www.agoogleaday.com/#date=2011-06-04")
+    time.sleep(1)
+
+elif rn == 28:
+    web.open("https://www.google.com/search?q=google+doodles&csf=b&ct=ifl&cad=2:doodley&ei=NuJEYYLeGMOf-Ab365yABw&ved=0ahUKEwiC0eql1YbzAhXDD94KHfc1B3AQnRsIDw&rct=j&safe=active&ssui=on")
+    time.sleep(1)
+
+elif rn == 29:
+    web.open("https://www.random.org/")
+    time.sleep(1)
+
+elif rn == 30:
+    web.open("https://www.officedepot.com/")
+    time.sleep(1)
